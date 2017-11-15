e6776b6c417477d31daf02088bf99ad971923066
Oleg Syrbu <olegsirbu2@gmail.com>
2017-11-15 15:53:51 +0200

    add search by news

---
+++ src/components/common/SearchInput.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import AutoComplete from 'material-ui/AutoComplete';
+
+export default class SearchInput extends Component {
+  constructor(props, context){
+    super(props, context);
+    this.state = {
+      searchText: ''
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if(this.props.dataSource.length !== nextProps.dataSource.length) {
+      const names = nextProps.dataSource.map((item)=>{return item.name})
+      this.setState({names: Object.assign([], names)});
+    }
+  }
+
+  handleUpdateInput = (searchText) => {
+    this.setState({
+      searchText: searchText,
+    });
+  };
+
+  handleNewRequest = (targetValue) => {
+    this.props.onSelect(targetValue);
+    this.setState({
+      searchText: '',
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <AutoComplete
+          hintText="Seatch"
+          searchText={this.state.searchText}
+          onUpdateInput={this.handleUpdateInput}
+          onNewRequest={this.handleNewRequest}
+          dataSource={this.props.dataSource}
+          filter={(searchText, key) => (key.indexOf(searchText) !== -1)}
+          openOnFocus={true}
+        />
+      </div>
+    );
+  }
+}
