8e3b1879b747ac64575ac6b26052d59213632aae
Oleg Syrbu <olegsirbu2@gmail.com>
2017-11-16 14:21:58 +0200

    refactor Notes component, add checkboxes

--- src/components/notes/NotesList.js
+++ src/components/notes/NotesList.js
@@ -1,22 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import NotesListRows from './NotesListRows';
+import {Link} from 'react-router';
 import {
   Table,
   TableBody,
   TableHeader,
   TableHeaderColumn,
-  TableRow
+  TableRow,
+  TableRowColumn
 } from 'material-ui/Table';
 
+const NodeRow = (note, index, {isSelectedHandler}) => {
+  return(
+    <TableRow key={note._id} selected={isSelectedHandler(index)}>
+      <TableRowColumn>{index}</TableRowColumn>
+      <TableRowColumn><Link to={'/note/'+note._id}>{note.title}</Link></TableRowColumn>
+      <TableRowColumn>{note.text}</TableRowColumn>
+    </TableRow>
+  )
+};
 
-const NoteList = ({ notes, isSelected }) => {
+const NoteList = ({ notes, handleRowSelection, ...props }) => {
   return (
+    <div>
     <Table
+      onRowSelection={handleRowSelection}
+      selectable={true}
+      multiSelectable={true}
     >
       <TableHeader
-        displaySelectAll={false}
-        adjustForCheckbox={false}
+        enableSelectAll={true}
       >
         <TableRow>
           <TableHeaderColumn>ID</TableHeaderColumn>
@@ -25,13 +38,12 @@ const NoteList = ({ notes, isSelected }) => {
         </TableRow>
       </TableHeader>
       <TableBody
-        displayRowCheckbox={false}
+        deselectOnClickaway={false}
       >
-        {notes.map((note,index)=>
-          <NotesListRows key={note.title+ '_' +note._id} note={note} index={index} isSelected={isSelected}/>
-        )}
+        {notes.map((note, index) => NodeRow(note, index, props))}
       </TableBody>
     </Table>
+    </div>
   );
 };
 
